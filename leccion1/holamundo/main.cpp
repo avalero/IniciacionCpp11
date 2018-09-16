d39c37e987af7b8c9110f4e78b826bea7111fe5a
Alberto Valero <alberto.valero@bq.com>
2018-09-16 14:40:23 +0200

    updated leccion 1

---
+++ leccion1/holamundo/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+string nombre{"Nieves"};
+unsigned short int edad{27};
+
+int main()
+{
+    cout << "Hola " << nombre <<", tienes " << edad << " años."<< endl;
+    return 0;
+}
